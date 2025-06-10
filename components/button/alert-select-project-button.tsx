493fc6e3d62ac4c6a4f286de7222717fcf7baaaa
Wahyu Esya <wahyu.esya17@gmail.com>
2025-06-10 10:47:30 +0700

    fix(Card) add max width for alert select project

--- components/button/alert-select-project-button.tsx
+++ components/button/alert-select-project-button.tsx
@@ -2,7 +2,7 @@ import { FaTriangleExclamation } from "react-icons/fa6";
 
 const AlertSelectProjectButton = () => {
   return (
-    <div className="tracking-wide bg-primary w-max py-1.5 px-4 rounded-sm text-white flex items-center gap-2">
+    <div className="tracking-wide bg-primary w-max py-1.5 px-4 rounded-sm text-white flex items-center gap-2 break-words whitespace-pre-wrap max-w-4/5 md:max-w-full">
       <FaTriangleExclamation />
       <span>Please select a project or create a new one first!</span>
     </div>
