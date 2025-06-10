c3cc4986217d9d7cae426565694e61591fb5e965
Wahyu Esya <wahyu.esya17@gmail.com>
2025-06-10 11:25:56 +0700

    add suspense for search params

--- components/input/form-filter.tsx
+++ components/input/form-filter.tsx
@@ -17,7 +17,7 @@ import { PriorityOptions } from "@/lib/data";
 import Badge from "../ui/badge";
 import { Button } from "../ui/button";
 import { FaMagnifyingGlass, FaSpinner } from "react-icons/fa6";
-import { useTransition } from "react";
+import { Suspense, useTransition } from "react";
 import { useFilter } from "@/store/useFilter";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
@@ -70,67 +70,76 @@ const FormFilter = () => {
   };
 
   return (
-    <Form {...form}>
-      <form
-        className="flex gap-2 md:gap-4 md:max-w-3xl"
-        onSubmit={form.handleSubmit(handleOnSubmit)}
-      >
-        <FormField
-          control={form.control}
-          name="search"
-          render={({ field }) => <Input placeholder="Search..." {...field} />}
-        />
-        <FormField
-          control={form.control}
-          name="priority"
-          render={({ field }) => (
-            <FormItem>
-              <Select
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                value={field.value}
-              >
-                <FormControl>
-                  <SelectTrigger className="cursor-pointer w-[140px]">
-                    <SelectValue placeholder="Select a priority" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="ALL" className="cursor-pointer">
-                    <Badge
-                      option={{
-                        value: "ALL",
-                        label: "ALL",
-                        color: "#64748B",
-                        bgColor: "bg-slate-50",
-                        textColor: "text-slate-700",
-                      }}
-                    />
-                  </SelectItem>
-                  {PriorityOptions.map((option) => (
-                    <SelectItem
-                      key={option.value}
-                      value={option.value}
-                      className="cursor-pointer"
-                    >
-                      <Badge option={option} />
+    <Suspense
+      fallback={
+        <div>
+          Loading
+          <FaSpinner className="ml-2 h-4 w-4 animate-spin" />
+        </div>
+      }
+    >
+      <Form {...form}>
+        <form
+          className="flex gap-2 md:gap-4 md:max-w-3xl"
+          onSubmit={form.handleSubmit(handleOnSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => <Input placeholder="Search..." {...field} />}
+          />
+          <FormField
+            control={form.control}
+            name="priority"
+            render={({ field }) => (
+              <FormItem>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  value={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className="cursor-pointer w-[140px]">
+                      <SelectValue placeholder="Select a priority" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="ALL" className="cursor-pointer">
+                      <Badge
+                        option={{
+                          value: "ALL",
+                          label: "ALL",
+                          color: "#64748B",
+                          bgColor: "bg-slate-50",
+                          textColor: "text-slate-700",
+                        }}
+                      />
                     </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="cursor-pointer" disabled={isPending}>
-          <span className="hidden sm:inline">Search</span>
-          {!isPending ? (
-            <FaMagnifyingGlass />
-          ) : (
-            <FaSpinner className="ml-2 h-4 w-4 animate-spin" />
-          )}
-        </Button>
-      </form>
-    </Form>
+                    {PriorityOptions.map((option) => (
+                      <SelectItem
+                        key={option.value}
+                        value={option.value}
+                        className="cursor-pointer"
+                      >
+                        <Badge option={option} />
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="cursor-pointer" disabled={isPending}>
+            <span className="hidden sm:inline">Search</span>
+            {!isPending ? (
+              <FaMagnifyingGlass />
+            ) : (
+              <FaSpinner className="ml-2 h-4 w-4 animate-spin" />
+            )}
+          </Button>
+        </form>
+      </Form>
+    </Suspense>
   );
 };
 
--- components/input/select-project.tsx
+++ components/input/select-project.tsx
@@ -10,7 +10,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import DialogCreateProject from "../dialog/dialog-create-project";
-import { useEffect, useState, useTransition } from "react";
+import { Suspense, useEffect, useState, useTransition } from "react";
 import { getProjectsByUserId } from "@/lib/action/action-project";
 import { FaSpinner } from "react-icons/fa6";
 import { useFilter } from "@/store/useFilter";
@@ -76,41 +76,50 @@ export const SelectProject = () => {
   }, []);
 
   return (
-    <Select
-      value={filter.project}
-      onValueChange={(value) => setFilter({ project: value })}
+    <Suspense
+      fallback={
+        <div>
+          Loading
+          <FaSpinner className="ml-2 h-4 w-4 animate-spin" />
+        </div>
+      }
     >
-      <SelectTrigger className="min-w-[150px] sm:min-w-[200px] cursor-pointer sm:ml-4">
-        {isPending ? (
-          <div className="flex items-center gap-2">
-            <FaSpinner className="h-4 w-4 animate-spin" />
-            <span>Loading projects...</span>
-          </div>
-        ) : (
-          <SelectValue placeholder="Select a Project" />
-        )}
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>List Projects</SelectLabel>
-          {projectsData.length === 0 && (
-            <p className="text-sm px-2">Empty projects</p>
+      <Select
+        value={filter.project}
+        onValueChange={(value) => setFilter({ project: value })}
+      >
+        <SelectTrigger className="min-w-[150px] sm:min-w-[200px] cursor-pointer sm:ml-4">
+          {isPending ? (
+            <div className="flex items-center gap-2">
+              <FaSpinner className="h-4 w-4 animate-spin" />
+              <span>Loading projects...</span>
+            </div>
+          ) : (
+            <SelectValue placeholder="Select a Project" />
           )}
-          {projectsData &&
-            projectsData.map((project) => (
-              <SelectItem
-                key={project.value}
-                value={project.value}
-                className="cursor-pointer"
-              >
-                <span className="size-3.5 bg-primary rounded-full" />
-                {project.label}
-              </SelectItem>
-            ))}
-          {projectsData.length <= 5 && <DialogCreateProject />}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>List Projects</SelectLabel>
+            {projectsData.length === 0 && (
+              <p className="text-sm px-2">Empty projects</p>
+            )}
+            {projectsData &&
+              projectsData.map((project) => (
+                <SelectItem
+                  key={project.value}
+                  value={project.value}
+                  className="cursor-pointer"
+                >
+                  <span className="size-3.5 bg-primary rounded-full" />
+                  {project.label}
+                </SelectItem>
+              ))}
+            {projectsData.length <= 5 && <DialogCreateProject />}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </Suspense>
   );
 };
 
